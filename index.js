57987b88c15059b47c2fa10d91a8475742cedb57
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-22 16:50:29 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
+var Linkedin = require('node-linkedin')(),
+    _ = require('lodash'),
+    util = require('./util.js');
+
+var pickInputs = {
+        'id': 'id',
+        'name': 'name',
+        'additional_fields': { key: 'additional_fields', type: 'array' }
+    };
+
 module.exports = {
+
+    /**
+     * Authorize module.
+     *
+     * @param dexter
+     * @returns {*}
+     */
+    authModule: function (dexter) {
+        var accessToken = dexter.environment('linkedin_access_token');
+
+        if (accessToken)
+            return Linkedin.init(accessToken);
+
+        else
+            return false;
+    },
+
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +34,22 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+        var linkedIn = this.authModule(dexter),
+            inputs = util.pickStringInputs(step, pickInputs);
+
+        if (!linkedIn)
+            return this.fail('A [linkedin_access_token] environment need for this module.');
+
+        linkedIn.companies.company(inputs.id, function(err, data) {
+            if (err)
+                this.fail(err);
+
+            else if (data.errorCode !== undefined)
+                this.fail(data.message || 'Error Code'.concat(data.errorCode));
+
+            else
+                this.complete(_.merge(_.pick(data, ['id', 'name']), { additional_fields: _.pick(data, inputs.additional_fields) }));
+
+        }.bind(this));
     }
 };
